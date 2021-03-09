a605e02f3b81ed48f970245adbfe0727eedb8562
William Moody <e11736947@student.tuwien.ac.at>
2021-03-09 23:03:19 +0100

    .

---
+++ .docker/emulate_admin.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# ------SPOILERS------
+# DO NOT READ THIS IF YOU HAVEN'T GOTTEN ACCESS TO THE ADMIN'S
+# ACCOUNT YET.
+
+# --------------------
+# Emulates admin actions necessary for privilege escalation 
+# from a basic user account to admin.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+opts = webdriver.FirefoxOptions()
+opts.add_argument("--headless")
+driver = webdriver.Firefox(firefox_options=opts)
+driver.get("http://localhost/login.php")
+assert "TUDO/Log In" in driver.title
+
+u_input = driver.find_element_by_name("username")
+p_input = driver.find_element_by_name("password")
+u_input.send_keys("admin")
+p_input.send_keys("admin")
+p_input.send_keys(Keys.RETURN)
+
+time.sleep(5)
+driver.close()
---
+++ .docker/entrypoint.sh
@@ -0,0 +1,21 @@
+# TUDO!!!!!
+echo
+echo
+echo "-=-=-=-[ TUDO ]-=-=-=-"
+echo 
+echo b
+
+# start and configure psql server
+/etc/init.d/postgresql start
+sudo -u postgres psql -c "CREATE DATABASE tudo"
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres'"
+sudo -u postgres psql -f /var/www/html/.docker/setup.sql tudo
+
+# start cron
+/etc/init.d/cron start
+
+# start apache server
+/usr/sbin/apache2ctl -D FOREGOUND
+
+# stayin alive
+/bin/bash -c 'while [[ 1 ]]; do sleep 60 || break; done';
